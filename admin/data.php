a7985bc055abc64188e9391dbd771975f26ad9c8
nhibeo <82997124+nhibeo@users.noreply.github.com>
2021-04-22 13:34:59 +0700

    conflicted

--- admin/data.php
+++ admin/data.php
@@ -3,7 +3,7 @@ define("DB_SERVER", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "rcdatabase");
-//da sua mot file nay  r nhe
+//co tinh de conflicted
 function db_connect(){
     $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
     return $connection;
