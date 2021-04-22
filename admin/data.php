36678706eaa87443b7baee9f63305eac071b4fa9
nhibeo <82997124+nhibeo@users.noreply.github.com>
2021-04-22 13:33:49 +0700

    file

--- admin/data.php
+++ admin/data.php
@@ -3,7 +3,7 @@ define("DB_SERVER", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "rcdatabase");
-//da sua roi dday
+//da sua mot file nay  r nhe
 function db_connect(){
     $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
     return $connection;
